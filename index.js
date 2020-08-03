c7e3dc999d656a9c7eeb537e1304966a86258ecc
Supphawit-J <ideajeng2543@gmail.com>
2020-08-03 15:04:26 +0700

    initial project

---
+++ index.js
@@ -0,0 +1,36 @@
+let var1;
+const var2=20;
+console.log(var2)
+
+let arr =[1,2,3];
+
+arr.push(4);
+
+
+for (let i=0;i<arr.length;i++){
+
+    console.log(arr[i])
+}
+
+const obj = {
+name:"jeng",
+age: 20,
+'key-1' : "OMG"
+
+
+};
+console.log(obj.age)
+console.log(obj.name)
+console.log(obj["key-1"])
+
+
+const obj2 = {
+arr: [ { name: Otis},20]
+}
+console.log(obj2.arr[0].name)
+
+function sum(param) {
+    
+}
+const func = param => param
+
